a454c78dca7e08348a9481bdd97803f52068cf61
Sarge <mdodge@pivotal.io>
2017-11-02 15:51:48 -0700

    Fix null pointer exception with SCXML engine.

--- src/io/pivotal/prototype/scxml/Main.java
+++ src/io/pivotal/prototype/scxml/Main.java
@@ -21,32 +21,51 @@ import org.apache.commons.scxml2.env.SimpleErrorReporter;
 import org.apache.commons.scxml2.env.SimpleSCXMLListener;
 import org.apache.commons.scxml2.env.SimpleXMLReporter;
 import org.apache.commons.scxml2.env.URLResolver;
+import org.apache.commons.scxml2.env.javascript.JSContext;
+import org.apache.commons.scxml2.env.javascript.JSEvaluator;
 import org.apache.commons.scxml2.env.minimal.MinimalEvaluator;
 import org.apache.commons.scxml2.io.SCXMLReader;
 import org.apache.commons.scxml2.model.CustomAction;
+import org.apache.commons.scxml2.model.Final;
 import org.apache.commons.scxml2.model.Log;
 import org.apache.commons.scxml2.model.ModelException;
 import org.apache.commons.scxml2.model.SCXML;
+import org.apache.commons.scxml2.model.SimpleTransition;
+import org.apache.commons.scxml2.model.Transition;
 
 public class Main extends ArgumentInvoker {
   public static void main(String[] args) {
     int status = 0;
     try {
       new Main().invoke(args);
-    }
-    catch (Exception e) {
+    } catch (Exception e) {
       status = 1;
       e.printStackTrace(System.err);
     }
     System.exit(status);
   }
 
-  public void write(String text) {
-    System.out.println(text);
-  }
-
-  public void increment(Integer i) {
-    System.out.println("i=" + (i + 1));
+  public void execute() {
+    try {
+      SCXML stateMachine = new SCXML();
+      final Final state = new Final();
+      stateMachine.addChild(state);
+      SimpleTransition transition = new SimpleTransition();
+      transition.setNext(state.getId());
+      stateMachine.setInitialTransition(new SimpleTransition());
+      Evaluator evaluator = new JSEvaluator();
+      EventDispatcher eventDispatcher = new SimpleDispatcher();
+      ErrorReporter errorReporter = new SimpleErrorReporter();
+      SCXMLExecutor exec = new SCXMLExecutor(evaluator, eventDispatcher, errorReporter);
+      exec.setStateMachine(stateMachine);
+      SCXMLListener listener = new SimpleSCXMLListener();
+      exec.addListener(stateMachine, listener);
+      Context context = new JSContext();
+      exec.setRootContext(context);
+      exec.go();
+    } catch (ModelException e) {
+      e.printStackTrace(System.err);
+    }
   }
 
   public void read(String url) {
@@ -71,22 +90,12 @@ public class Main extends ArgumentInvoker {
     }
   }
 
-  public void execute() {
-    try {
-      SCXML stateMachine = new SCXML();
-      Evaluator evaluator = new MinimalEvaluator();
-      EventDispatcher eventDispatcher = new SimpleDispatcher();
-      ErrorReporter errorReporter = new SimpleErrorReporter();
-      SCXMLExecutor exec = new SCXMLExecutor(evaluator, eventDispatcher, errorReporter);
-      exec.setStateMachine(stateMachine);
-      SCXMLListener listener = new SimpleSCXMLListener();
-      exec.addListener(stateMachine, listener);
-      Context context = new SimpleContext();
-      exec.setRootContext(context);
-      exec.go();
-    } catch (ModelException e) {
-      e.printStackTrace(System.err);
-    }
+  public void increment(Integer i) {
+    System.out.println("i=" + (i + 1));
+  }
+
+  public void write(String text) {
+    System.out.println(text);
   }
 
   @Override
