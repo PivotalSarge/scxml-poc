1eb6d441cbdd410a4ec10303f7f1c3b40e6f5c4b
Sarge <mdodge@pivotal.io>
2017-11-02 15:51:10 -0700

    Fix directory name for build output from IntelliJ.

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -cp out/production/scxml:/usr/local/lib/commons-scxml2.jar:/usr/local/lib/commons-logging-1.2.jar io.pivotal.prototype.scxml.Main $*
+java -cp out/production/scxml-poc:/usr/local/lib/commons-scxml2.jar:/usr/local/lib/commons-logging-1.2.jar io.pivotal.prototype.scxml.Main $*
